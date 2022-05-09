4b35b9f758af7144f66b420fd23eecf7ac4c00c1
lzn <482087021@qq.com>
2022-05-09 18:06:32 +0800

    成功在注册用户时建表

--- src/main/java/com/example/lifehealthassistant/dao/UserInfoDao.java
+++ src/main/java/com/example/lifehealthassistant/dao/UserInfoDao.java
@@ -3,10 +3,24 @@ package com.example.lifehealthassistant.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.lifehealthassistant.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface UserInfoDao extends BaseMapper<User> {
+
+    @Update("create table #{id}_diet" +
+            "(" +
+            "date_time datetime not null," +
+            "food VARCHAR(255)," +
+            "dietname VARCHAR(255)," +
+            "picture1 VARCHAR(255)," +
+            "picture2 VARCHAR(255)," +
+            "picture3 VARCHAR(255)," +
+            "PRIMARY KEY(date_time,dietname)" +
+            ");")
+    public boolean save_create(User user);
+
 
 }
--- src/main/java/com/example/lifehealthassistant/service/UserInfoServiceImpl.java
+++ src/main/java/com/example/lifehealthassistant/service/UserInfoServiceImpl.java
@@ -3,10 +3,19 @@ package com.example.lifehealthassistant.service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.lifehealthassistant.dao.UserInfoDao;
 import com.example.lifehealthassistant.domain.User;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoDao, User> implements UserInfoService{
 
-    
+    @Autowired
+    UserInfoDao userInfoDao;
+
+    @Override
+    public boolean save(User user) {
+        userInfoDao.save_create(user);
+        return (userInfoDao.insert(user)>0);
+    }
 }
